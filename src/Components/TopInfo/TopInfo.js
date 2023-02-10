4bcd73fd44294682ebfbe71bf501b51139af3660
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-02-10 22:22:52 +0600

    Top Info Added

---
+++ src/Components/TopInfo/TopInfo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopInfo = () => {
+    return (
+        <div className='bg-[#040F16] h-12 flex justify-center items-center'>
+            <p className='text-xs m-0 text-white '>We’ve extended our returns period to 60 days</p>
+        </div>
+    );
+};
+
+export default TopInfo;
