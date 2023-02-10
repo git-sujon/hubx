95a8d77ef2309edf588bbe045f82fe2390115d97
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-02-10 16:48:18 +0600

    Initail Files and Folders are created

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXhyDV5sLo9AhB9K8xDBK0MFxM6LZRCHI",
+  authDomain: "hubx-5e21c.firebaseapp.com",
+  projectId: "hubx-5e21c",
+  storageBucket: "hubx-5e21c.appspot.com",
+  messagingSenderId: "819709517337",
+  appId: "1:819709517337:web:afaa928200b89e7a1c5488"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/Routes/Routes.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
+import Home from '../Pages/Home/Home';
+import LogIn from '../Pages/LogIn/LogIn';
+import ResetPassword from '../Pages/ResetPassword/ResetPassword';
+import SignUp from '../Pages/SignUp/SignUp';
+import UserProfiles from '../Pages/UserProfiles/UserProfiles';
+
+
+const Routes =createBrowserRouter([
+  {
+    path:'/',
+    element: <Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'singUp',
+        element:<SignUp></SignUp>
+      },
+      {
+        path:'login',
+        element:<LogIn></LogIn>
+      },
+      {
+        path:'resetPassword',
+        element:<ResetPassword></ResetPassword>
+      },
+      {
+        path:'userProfiles',
+        element:<UserProfiles></UserProfiles>
+      },
+    ]
+  }
+])
+
+export default Routes;
