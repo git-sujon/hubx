ca99eca6c5aee859c825d3538aa622d55bd6f306
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-02-11 21:21:51 +0600

    Button and link components added

--- src/Components/Link/CommonLink.js
+++ src/Components/Link/CommonLink.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 
 const CommonLink = ({to, children}) => {
     return (
-        
+        <Link to={to} class="text-sm text-[#2554D7]  hover:font-bold">
+        {children}
+      </Link>
     );
 };
 
