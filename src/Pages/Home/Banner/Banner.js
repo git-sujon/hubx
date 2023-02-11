1eb4f44ee20b04ce3fe8e0de0666314e37424e23
Moniruzzaman Sujon <git.sujon@gmail.com>
2023-02-11 22:46:05 +0600

    Banner Added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import heroImage from '../../../Assets/Images/Banner/Hero.png'
+import React from "react";
+import heroImage from "../../../Assets/Images/Banner/Hero.png";
+import Button from "../../../Components/Button/Button";
 
 const Banner = () => {
-    return (
-        <div></div>
-      
-    );
+  return (
+    <section
+      class="bg-cover bg-center bg-no-repeat relative"
+      style={{ backgroundImage: `url(${heroImage})` }}
+    >
+     
+     <div class="absolute inset-0 bg-black/25 "></div>
+      <div class="mx-auto  px-4 py-32 lg:flex  lg:items-center">
+        <div class="mx-auto max-w-[800px] text-center">
+          <h1 class="text-3xl font-bold sm:text-8xl text-white rajdhani-font">
+            Sports Clothing & Fitness Equipment Shopify Theme
+          </h1>
+
+          <div class="mt-8 flex flex-wrap justify-center gap-4">
+            <Button>View Shop</Button>
+          </div>
+        </div>
+      </div>
+
+    </section>
+  );
 };
 
 export default Banner;
